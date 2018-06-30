49896f74327ba89b285aeb3080a9e77b19953fe1
nanlei2000 <1456958184@qq.com>
2018-06-30 09:29:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+### a quite simple router implemented with ES6 syntax 
+
+you can initialize a router using a entrance node
+
+``const router = new Router("#app")``
+
+then,set a homepage goes like:
+
+      router.route('', () => {
+            router.go('home')
+         })
+      router.route('home', () => {
+         router.render('this is home')
+      })
+
+use a substring to create serial routes ,if the hash string include the substring you set,then callback execute   
+if you set the same hash for ``router.route()`` and ``router.match()`` ,the callback for ``router.route()`` will not execute
+
+      router.route('show',function(){
+            router.render('this is show')
+            //you will nerver see this...
+         })
+      router.match('show',function(){
+         router.render('this is show also')
+      })
---
+++ index.js
@@ -0,0 +1,56 @@
+/**
+ * Created by @nanlei2000 in 2018-06-30 09:27:25
+ */
+
+class Router {
+   constructor(entrance) {
+      this.entrance = document.querySelector(entrance)
+      this.routes = new Map()
+      this.subStrMap = new Map()
+      let noop = () => {}
+      this.routes.set('', noop)
+      this._active()
+   }
+   route(hash, fn) {
+      this.routes.set(hash, fn)
+   }
+   match(subStr, fn) {
+      this.subStrMap.set(subStr, fn)
+   }
+   _active() {
+      window.addEventListener('load', this._run.bind(this))
+      window.addEventListener('hashchange', this._run.bind(this))
+      window.addEventListener('load', this._runMatch.bind(this))
+      window.addEventListener('hashchange', this._runMatch.bind(this))
+   }
+   _run() {
+      let hash = location.hash.substr(1)
+      if (this.routes.get(hash) === undefined) {
+         this.render('')
+      } else {
+         this.routes.get(hash)()
+      }
+   }
+   _runMatch() {
+      let hash = location.hash.substr(1)
+      if (this.subStrMap.size !== 0) {
+         for (let key of this.subStrMap.keys()) {
+            if (hash.indexOf(key) !== -1) {
+               this.subStrMap.get(key)()
+               break
+            } else {
+               this.render('')
+            }
+         }
+      }
+   }
+   render(html) {
+      this.entrance.innerHTML = html
+   }
+   go(hash) {
+      location.hash = hash
+   }
+   back() {
+      history.go(-1)
+   }
+}
